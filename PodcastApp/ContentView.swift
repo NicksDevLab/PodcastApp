5867813504b13af32c06a097597a69fe63a90e4a
Nicholas Church <nicholaschurch@ymail.com>
2020-06-06 07:14:41 -0400

    Initial Commit

---
+++ PodcastApp/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  PodcastApp
+//
+//  Created by Nicholas Church on 6/6/20.
+//  Copyright © 2020 Nicholas Church. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
